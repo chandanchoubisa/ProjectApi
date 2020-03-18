8b6db32d204ae237969036b635cbfa5f53b8e5e5
chandanchoubisa <chandanchoubisa24@gmail.com>
2020-03-18 10:29:16 +0530

    Added Tests for User model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test Creating a new user with an email is successful"""
+        email = 'test@chandan.com'
+        password = 'Chandan'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_normalize_email_of_user(self):
+        """Test for Normalize email in lower case"""
+        email = "test@CHANDAN.COM"
+        user = get_user_model().objects.create_user(email, 'Chandan')
+
+        self.assertEqual(user.email,email.lower())
+
+
+    def test_user_invalid_email(self):
+        """Test for invalid email raises Value error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
+
+
+    def test_create_new_superuser(self):
+        """For Creating new Superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
